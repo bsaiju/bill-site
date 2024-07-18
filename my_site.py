635acaf287a7aef202e83ec359fca6dcd211c01f
Bharat Saiju <bsaiju@ucsb.edu>
2024-07-18 14:56:20 -0700

    search bar functionality on home page/not much css for it yet tho

--- my_site.py
+++ my_site.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for
 from forms import RegisterForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '271b1f5887fa7ecd25ccc86bd0a0e338'
-data = [["name", "phone"], ["bob","234"], ["james","2313"]]
+data = [["name", "phone"], ["bob","234"], ["james","2313"], ["hope", "232"], ["uliop", "6758"], ["phoebe", "234534"]]
 @app.route("/home")
 def home():
     return render_template('home.html', data=data)
